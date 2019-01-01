06e584c3f93450c5dea54b4afcbd14f82e29299f
jaynilshah <jaynil.shah84@gmail.com>
2019-01-02 03:29:01 +0530

    Add mustache.js for templates

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ io.on('connection',(socket)=>{
     });
 
     socket.on('createLocationMessage',(coords)=>{
-        io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude));
+        io.emit('newLocationMessage',generateLocationMessage('User',coords.latitude,coords.longitude));
     })
 
     socket.on('disconnect',(socket)=>{
