2970d56fbdfefd44f0e8fc7cbe41817afeedf164
jaynilshah <jaynil.shah84@gmail.com>
2019-01-03 16:30:23 +0530

    Send messsages to people in same room only

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ var users = new Users();
 app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
-    console.log('new user connected');
+    // console.log('new user connected');
     
     
 
@@ -42,14 +42,20 @@ io.on('connection',(socket)=>{
     
     
     socket.on('createMessage',(message,callback)=>{
-        console.log(message);
-        io.emit('newMessage',generateMessage(message.from,message.text));
-        callback();
+        var user = users.getUser(socket.id);
+        if(user && isRealString(message.text)){
+        
+            io.to(user.room).emit('newMessage',generateMessage(user.name,message.text));
+            callback();
+    }
         
     });
 
     socket.on('createLocationMessage',(coords)=>{
-        io.emit('newLocationMessage',generateLocationMessage('User',coords.latitude,coords.longitude));
+        var user = users.getUser(socket.id);
+        if(user){
+        io.to(user.room).emit('newLocationMessage',generateLocationMessage(user.name,coords.latitude,coords.longitude));
+        }
     })
 
     socket.on('disconnect',()=>{
@@ -60,7 +66,7 @@ io.on('connection',(socket)=>{
             io.to(user.room).emit('updateUserList',users.getUserList(user.room));
             io.to(user.room).emit('newMessage',generateMessage('Admin',`${user.name} left`));
         }
-        console.log('User disconnected');
+        // console.log('User disconnected');
     });
 });
 
